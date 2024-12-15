63cc5bd062ba98a809a7c4c580f42deb321cde67
maluda <nwitevictor7@gmail.com>
2024-12-15 04:22:10 +0100

    updated app.js and inserted utils/database.ts

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectToDb } from "./utils/database.js";
+
+dotenv.config();
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3500;
+
+connectToDb();
+
+app.listen(PORT, (req, res) => {
+  console.log(`listening on port ${PORT}`);
+});
